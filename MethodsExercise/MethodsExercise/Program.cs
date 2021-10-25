d7f67986a44a7bccb62832ac051a7cc6ea0ebe8b
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-25 11:00:52 -0500

    math methods added

--- MethodsExercise/MethodsExercise/Program.cs
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MethodsExercise
 {
@@ -15,12 +16,47 @@ namespace MethodsExercise
             Console.WriteLine("What id your favorite band?");
             string favBand = Console.ReadLine();
 
-            myStory(userName, favColor, favAnimal, favBand);
+            MyStory(userName, favColor, favAnimal, favBand);
 
-            static void myStory(string userName, string favColor, string favAnimal, string favBand)
+            Console.WriteLine("Enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] number = input.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+
+            Console.WriteLine($"The sum of all digits is:");
+            Add(number);
+            Console.WriteLine($"The subtracted value of all digits is:");
+            Sub(number);
+            Console.WriteLine($"The product of all digits is:");
+            Mult(number);
+            Console.WriteLine($"The remainder of all digits is:");
+            Div(number);
+            Console.WriteLine($"The modulus of all digits is:");
+            Mod(number);
+
+            static void MyStory(string userName, string favColor, string favAnimal, string favBand)
             {
                string result = $"{userName} Macdonald had a {favAnimal}, E-I-E-I-O\nOn that farm they had a {favColor} {favBand},E-I-E-I-O";
                 Console.WriteLine(result);
+            }
+            static void Add(params int[] number)
+            {
+                Console.WriteLine(number.Sum());
+            }
+            static void Sub(params int[] number)
+            {
+                Console.WriteLine(number.Aggregate(1, (a, b) => a - b));
+            }
+            static void Mult(params int[] number)
+            {
+                Console.WriteLine(number.Aggregate(1, (a, b) => a * b));
+            }
+            static void Div(params int[] number)
+            {
+                Console.WriteLine(number.Aggregate(1, (a, b) => a / b));
+            }
+            static void Mod(params int[] number)
+            {
+                Console.WriteLine(number.Aggregate(1, (a, b) => a % b));
             }
 
         }
